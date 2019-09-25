0783e9767f3ddf5cdd7440ef0c2369e79578e1fa
miferreiro <miguel.ferreiro.diaz@gmail.com>
2019-09-25 14:19:31 +0200

    Adds bdparData example

---
+++ R/bdparData.R
@@ -0,0 +1,14 @@
+#' Example of the content of the files to be preprocessed.
+#'
+#' A manually collected data set containing e-mails and SMS messages from the
+#' nutritional and health domain classified as spam and non-spam (with a ratio
+#' of 50\%). In addition the dataset contains two variables: (i) path which
+#' indicates the location of the target file and, (ii) source which contains
+#' the raw text comprising each file.
+#'
+#' @format A data frame with 20 rows and 2 variables:
+#' \describe{
+#'   \item{path}{File path.}
+#'   \item{source}{File content.}
+#' }
+"bdparData"
