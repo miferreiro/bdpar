b500eededae518887686aec8c6fb9f35068fcea7
miferreiro <miguel.ferreiro.diaz@gmail.com>
2020-01-30 11:36:56 +0100

    Updates the date of the license

--- R/bdparData.R
+++ R/bdparData.R
@@ -1,3 +1,26 @@
+#
+# Bdpar provide a tool to easily build customized data flows to pre-process
+# large volumes of information from different sources. To this end, bdpar allows
+# to (i) easily use and create new functionalities and (ii) develop new data
+# source extractors according to the user needs. Additionally, the package
+# provides by default a predefined data flow to extract and preprocess the most
+# relevant information (tokens, dates, ... ) from some textual sources (SMS,
+# email, tweets, YouTube comments).
+#
+# Copyright (C) 2020 Sing Group (University of Vigo)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>
+
 #' Example of the content of the files to be preprocessed.
 #'
 #' A manually collected data set containing e-mails and SMS messages from the
